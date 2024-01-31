628b70e4b537dfe33e006e562ae0e38058656408
Joshua Taubel <josh.taubel@gmail.com>
2024-01-31 13:18:08 -0600

    Fixed missing $

--- GUI-Automation-Template.ps1
+++ GUI-Automation-Template.ps1
@@ -107,11 +107,11 @@ class Pixel {
     [int] $Y
     [String] $Color
 
-    Pixel([hashtable]$Properties) {this.Init($Properties)}
+    Pixel([hashtable]$Properties) {$this.Init($Properties)}
 
     [void] Init([hashtable]$Properties) {
         foreach ($Property in $Properties.Keys) {
-            this.$Property = $Properties.$Property
+            $this.$Property = $Properties.$Property
         }
     }
 }
