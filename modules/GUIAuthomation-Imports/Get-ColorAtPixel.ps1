357f6b83e0c68879da1b36f67a01b7125042bfe6
Joshua Taubel <josh.taubel@gmail.com>
2024-02-08 22:50:10 -0600

    added optional debug mode to get the png

--- modules/GUIAuthomation-Imports/Get-ColorAtPixel.ps1
+++ modules/GUIAuthomation-Imports/Get-ColorAtPixel.ps1
@@ -5,37 +5,38 @@ using module ..\pixel\pixel.psm1
 function Get-ColorAtPixel {
     param (
         [Pixel] $pixel,
-        [System.Drawing.Bitmap] $bitmap = [System.Drawing.Bitmap](Get-ScreenBitmap)
+        [System.Drawing.Bitmap] $bitmap = [System.Drawing.Bitmap](Get-ScreenBitmap),
+        [bool] $debug = $false
     )
     #this is to debug the pixel is that is being targeted
-    
-    $red = [System.Drawing.Color]::Red
-    $bitmap.SetPixel($pixel.X-2, $pixel.Y-2, $red)
-    $bitmap.SetPixel($pixel.X-2, $pixel.Y-1, $red)
-    $bitmap.SetPixel($pixel.X-2, $pixel.Y, $red)
-    $bitmap.SetPixel($pixel.X-2, $pixel.Y+1, $red)
-    $bitmap.SetPixel($pixel.X-2, $pixel.Y+2, $red)
-    $bitmap.SetPixel($pixel.X-1, $pixel.Y-2, $red)
-    $bitmap.SetPixel($pixel.X-1, $pixel.Y-1, $red)
-    $bitmap.SetPixel($pixel.X-1, $pixel.Y, $red)
-    $bitmap.SetPixel($pixel.X-1, $pixel.Y+1, $red)
-    $bitmap.SetPixel($pixel.X-1, $pixel.Y+2, $red)
-    $bitmap.SetPixel($pixel.X, $pixel.Y-2, $red)
-    $bitmap.SetPixel($pixel.X, $pixel.Y-1, $red)
-    $bitmap.SetPixel($pixel.X, $pixel.Y+1, $red)
-    $bitmap.SetPixel($pixel.X, $pixel.Y+2, $red)
-    $bitmap.SetPixel($pixel.X+1, $pixel.Y-2, $red)
-    $bitmap.SetPixel($pixel.X+1, $pixel.Y-1, $red)
-    $bitmap.SetPixel($pixel.X+1, $pixel.Y, $red)
-    $bitmap.SetPixel($pixel.X+1, $pixel.Y+1, $red)
-    $bitmap.SetPixel($pixel.X+1, $pixel.Y+2, $red)
-    $bitmap.SetPixel($pixel.X+2, $pixel.Y-2, $red)
-    $bitmap.SetPixel($pixel.X+2, $pixel.Y-1, $red)
-    $bitmap.SetPixel($pixel.X+2, $pixel.Y, $red)
-    $bitmap.SetPixel($pixel.X+2, $pixel.Y+1, $red)
-    $bitmap.SetPixel($pixel.X+2, $pixel.Y+2, $red)
-    $bitMap.save("C:\Users\John of the Cross\Desktop\scripts\GUI-Automation\bitmap.png")
-    #>
+    if ($debug -eq $true) {
+        $red = [System.Drawing.Color]::Red
+        $bitmap.SetPixel($pixel.X-2, $pixel.Y-2, $red)
+        $bitmap.SetPixel($pixel.X-2, $pixel.Y-1, $red)
+        $bitmap.SetPixel($pixel.X-2, $pixel.Y, $red)
+        $bitmap.SetPixel($pixel.X-2, $pixel.Y+1, $red)
+        $bitmap.SetPixel($pixel.X-2, $pixel.Y+2, $red)
+        $bitmap.SetPixel($pixel.X-1, $pixel.Y-2, $red)
+        $bitmap.SetPixel($pixel.X-1, $pixel.Y-1, $red)
+        $bitmap.SetPixel($pixel.X-1, $pixel.Y, $red)
+        $bitmap.SetPixel($pixel.X-1, $pixel.Y+1, $red)
+        $bitmap.SetPixel($pixel.X-1, $pixel.Y+2, $red)
+        $bitmap.SetPixel($pixel.X, $pixel.Y-2, $red)
+        $bitmap.SetPixel($pixel.X, $pixel.Y-1, $red)
+        $bitmap.SetPixel($pixel.X, $pixel.Y+1, $red)
+        $bitmap.SetPixel($pixel.X, $pixel.Y+2, $red)
+        $bitmap.SetPixel($pixel.X+1, $pixel.Y-2, $red)
+        $bitmap.SetPixel($pixel.X+1, $pixel.Y-1, $red)
+        $bitmap.SetPixel($pixel.X+1, $pixel.Y, $red)
+        $bitmap.SetPixel($pixel.X+1, $pixel.Y+1, $red)
+        $bitmap.SetPixel($pixel.X+1, $pixel.Y+2, $red)
+        $bitmap.SetPixel($pixel.X+2, $pixel.Y-2, $red)
+        $bitmap.SetPixel($pixel.X+2, $pixel.Y-1, $red)
+        $bitmap.SetPixel($pixel.X+2, $pixel.Y, $red)
+        $bitmap.SetPixel($pixel.X+2, $pixel.Y+1, $red)
+        $bitmap.SetPixel($pixel.X+2, $pixel.Y+2, $red)
+        $bitMap.save("C:\Users\John of the Cross\Desktop\scripts\GUI-Automation\bitmap.png")
+    }
     $color = $bitmap.GetPixel($pixel.X, $pixel.Y)
     $color.Name
 }
